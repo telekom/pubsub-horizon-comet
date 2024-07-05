62a1bb310efdf91b3b6ee7474c058c6451693769
Jens Fischer <jens-fischer@telekom.de>
2024-07-05 09:48:13 +0200

    feat: use json based cache in CircuitBreakerCacheService

--- src/main/java/de/telekom/horizon/comet/service/CircuitBreakerCacheService.java
+++ src/main/java/de/telekom/horizon/comet/service/CircuitBreakerCacheService.java
@@ -5,11 +5,16 @@
 package de.telekom.horizon.comet.service;
 
 import com.hazelcast.core.HazelcastInstanceNotActiveException;
-import de.telekom.eni.pandora.horizon.cache.service.CacheService;
+import de.telekom.eni.pandora.horizon.cache.service.JsonCacheService;
+import de.telekom.eni.pandora.horizon.exception.JsonCacheException;
 import de.telekom.eni.pandora.horizon.model.meta.CircuitBreakerMessage;
 import de.telekom.eni.pandora.horizon.model.meta.CircuitBreakerStatus;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.Date;
 
 /**
  * The {@code CircuitBreakerCacheService} class provides a service for interacting with a circuit breaker
@@ -21,12 +26,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class CircuitBreakerCacheService {
 
     /**
      * The cache service used to store and retrieve circuit breaker status.
      */
-    private final CacheService circuitBreakerCache;
+    private final JsonCacheService<CircuitBreakerMessage> circuitBreakerCache;
 
     /**
      * Checks if the circuit breaker is open for the given subscription id.
@@ -36,11 +42,14 @@ public class CircuitBreakerCacheService {
      * @throws HazelcastInstanceNotActiveException if the hazelcast instance is not active
      */
     public boolean isCircuitBreakerOpenOrChecking(String subscriptionId) throws HazelcastInstanceNotActiveException {
-        var result = circuitBreakerCache.get(subscriptionId);
-        if (result.isPresent()) {
-            CircuitBreakerMessage circuitBreakerMessage = (CircuitBreakerMessage) result.get();
-            return CircuitBreakerStatus.OPEN.equals(circuitBreakerMessage.getStatus())
-                    || CircuitBreakerStatus.CHECKING.equals(circuitBreakerMessage.getStatus());
+        try {
+            var result = circuitBreakerCache.getByKey(subscriptionId);
+            if (result.isPresent()) {
+                CircuitBreakerMessage circuitBreakerMessage = result.get();
+                return CircuitBreakerStatus.OPEN.equals(circuitBreakerMessage.getStatus());
+            }
+        } catch (JsonCacheException e) {
+            log.error("Could not check status of circuit breaker for subscriptionId {}: {}", subscriptionId, e.getMessage());
         }
         return false;
     }
@@ -50,12 +59,23 @@ public class CircuitBreakerCacheService {
      * and environment information.
      *
      * @param subscriptionId The subscriptionId for which to open the circuit breaker.
-     * @param callbackUrl    The callback URL to be associated with the circuit breaker.
      * @param environment    The environment for which to open the circuit breaker.
      * @throws HazelcastInstanceNotActiveException if the hazelcast instance is not active
      */
-    public void openCircuitBreaker(String subscriptionId, String callbackUrl, String environment) throws HazelcastInstanceNotActiveException {
-        var circuitBreakerMessage = new CircuitBreakerMessage(subscriptionId, CircuitBreakerStatus.OPEN, callbackUrl, environment);
-        circuitBreakerCache.update(subscriptionId, circuitBreakerMessage);
+    public void openCircuitBreaker(String subscriptionId, String originMessageId, String environment) throws HazelcastInstanceNotActiveException {
+        try {
+            var circuitBreakerMessage = new CircuitBreakerMessage(
+                    subscriptionId,
+                    Date.from(Instant.now()),
+                    originMessageId,
+                    CircuitBreakerStatus.OPEN,
+                    environment,
+                    null,
+                    0
+            );
+            circuitBreakerCache.set(subscriptionId, circuitBreakerMessage);
+        } catch (JsonCacheException e) {
+            log.error("Could not open circuit breaker for subscriptionId {}: {}", subscriptionId, e.getMessage());
+        }
     }
 }
--- src/test/java/de/telekom/horizon/comet/service/CircuitBreakerCacheServiceTest.java
+++ src/test/java/de/telekom/horizon/comet/service/CircuitBreakerCacheServiceTest.java
@@ -4,7 +4,8 @@
 
 package de.telekom.horizon.comet.service;
 
-import de.telekom.eni.pandora.horizon.cache.service.CacheService;
+import de.telekom.eni.pandora.horizon.cache.service.JsonCacheService;
+import de.telekom.eni.pandora.horizon.exception.JsonCacheException;
 import de.telekom.eni.pandora.horizon.model.meta.CircuitBreakerMessage;
 import de.telekom.eni.pandora.horizon.model.meta.CircuitBreakerStatus;
 import de.telekom.horizon.comet.test.utils.ObjectGenerator;
@@ -15,6 +16,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.Instant;
+import java.util.Date;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -24,7 +27,7 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class CircuitBreakerCacheServiceTest {
     @Mock
-    CacheService cacheService;
+    JsonCacheService<CircuitBreakerMessage> cacheService;
     CircuitBreakerCacheService circuitBreakerCacheService;
 
     @BeforeEach
@@ -34,28 +37,36 @@ class CircuitBreakerCacheServiceTest {
 
     @Test
     @DisplayName("Write CircuitBreakerMessage in Cache")
-    void writeCircuitBreakerMessage() {
-        circuitBreakerCacheService.openCircuitBreaker(ObjectGenerator.TEST_SUBSCRIPTION_ID, "https://test.de", ObjectGenerator.TEST_ENVIRONMENT);
+    void writeCircuitBreakerMessage() throws JsonCacheException {
+        circuitBreakerCacheService.openCircuitBreaker(ObjectGenerator.TEST_SUBSCRIPTION_ID, "test", ObjectGenerator.TEST_ENVIRONMENT);
 
-        verify(cacheService, times(1)).update(eq(ObjectGenerator.TEST_SUBSCRIPTION_ID), any());
+        verify(cacheService, times(1)).set(eq(ObjectGenerator.TEST_SUBSCRIPTION_ID), any());
     }
 
     @Test
     @DisplayName("Return true when CircuitBreaker is open")
-    void returnTrueWhenCircuitBreakerOpen() {
-        var circuitBreakerMessage = new CircuitBreakerMessage(ObjectGenerator.TEST_SUBSCRIPTION_ID, CircuitBreakerStatus.OPEN, "https://test.de", ObjectGenerator.TEST_ENVIRONMENT);
-        when(cacheService.get(eq(ObjectGenerator.TEST_SUBSCRIPTION_ID))).thenReturn(Optional.of(circuitBreakerMessage));
+    void returnTrueWhenCircuitBreakerOpen() throws JsonCacheException {
+        var circuitBreakerMessage = new CircuitBreakerMessage(
+                ObjectGenerator.TEST_SUBSCRIPTION_ID,
+                Date.from(Instant.now()),
+                "test",
+                CircuitBreakerStatus.OPEN,
+                ObjectGenerator.TEST_ENVIRONMENT,
+                null,
+                0
+        );
+        when(cacheService.getByKey(eq(ObjectGenerator.TEST_SUBSCRIPTION_ID))).thenReturn(Optional.of(circuitBreakerMessage));
 
         assertTrue(circuitBreakerCacheService.isCircuitBreakerOpenOrChecking(ObjectGenerator.TEST_SUBSCRIPTION_ID));
-        verify(cacheService, times(1)).get(ObjectGenerator.TEST_SUBSCRIPTION_ID);
+        verify(cacheService, times(1)).getByKey(ObjectGenerator.TEST_SUBSCRIPTION_ID);
     }
 
     @Test
     @DisplayName("Return false when no CircuitBreaker is open")
-    void returnFalseWhenNoCircuitBreakerOpen() {
-        when(cacheService.get(eq(ObjectGenerator.TEST_SUBSCRIPTION_ID))).thenReturn(Optional.empty());
+    void returnFalseWhenNoCircuitBreakerOpen() throws JsonCacheException {
+        when(cacheService.getByKey(eq(ObjectGenerator.TEST_SUBSCRIPTION_ID))).thenReturn(Optional.empty());
 
         assertFalse(circuitBreakerCacheService.isCircuitBreakerOpenOrChecking(ObjectGenerator.TEST_SUBSCRIPTION_ID));
-        verify(cacheService, times(1)).get(ObjectGenerator.TEST_SUBSCRIPTION_ID);
+        verify(cacheService, times(1)).getByKey(ObjectGenerator.TEST_SUBSCRIPTION_ID);
     }
 }
