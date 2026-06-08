0eab8986f7a243ba433fe66bed068c0e7dd5f257
Jens Fischer <jens-fischer@telekom.de>
2026-06-08 18:13:24 +0200

    test: update tests for take additional retry into account

--- src/test/java/de/telekom/horizon/comet/service/CircuitBreakerTests.java
+++ src/test/java/de/telekom/horizon/comet/service/CircuitBreakerTests.java
@@ -79,7 +79,7 @@ class CircuitBreakerTests extends AbstractIntegrationTest {
         await().atMost(Duration.ofSeconds(15)).until(() -> circuitBreakerCacheService.isCircuitBreakerOpenOrChecking(subscriptionId));
         // we tried 3 times calling callback
         wireMockServer.verify(
-                exactly(3),
+                exactly(6),
                 postRequestedFor(
                         urlPathEqualTo(callbackPath)
                 )
